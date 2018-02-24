db24a9619250d8f3e03e4b5f2a4a0275da3fcce8
Dan Humphrey <danh2000@gmail.com>
2018-02-24 14:58:24 +1100

    initial project structure and BaseTestSuite

---
+++ ThirtyFour.UI/ThirtyFour.UI.Tests/Tests/BaseTestSuite.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyFour.UI.Tests.Tests
+{
+    [TestClass]
+    public abstract class BaseTestSuite
+    {
+        public TestContext TestContext { get; set; }
+
+        static IWebDriver DRIVER_INSTANCE;
+        protected IWebDriver driver;
+
+        protected abstract string Url { get; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+
+            ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            DRIVER_INSTANCE = new ChromeDriver(chromeService, chromeOptions);
+            DRIVER_INSTANCE.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        public BaseTestSuite()
+        {
+            driver = DRIVER_INSTANCE;
+        }
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(Url);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (TestContext.CurrentTestOutcome != UnitTestOutcome.Passed)
+            {
+                if (!Directory.Exists(TestContext.TestResultsDirectory))
+                {
+                    Directory.CreateDirectory(TestContext.TestResultsDirectory);
+                }
+
+                Screenshot screenShot = ((ITakesScreenshot)driver).GetScreenshot();
+                string fileName = TestContext.TestResultsDirectory + "\\Screenshot_" + TestContext.TestName + DateTime.Now.ToString("yyyy-dd-MM-HH-mm-ss") + ".png";
+                screenShot.SaveAsFile((fileName), ScreenshotImageFormat.Png);
+                TestContext.AddResultFile(fileName);
+            }
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            DRIVER_INSTANCE.Quit();
+        }
+    }
+}
