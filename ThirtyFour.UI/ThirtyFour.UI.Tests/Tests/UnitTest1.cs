21409d339b682c3374f89f79daca46fc0d82bae0
Dan Humphrey <danh2000@gmail.com>
2018-02-24 14:58:24 +1100

    initial project structure and BaseTestSuite

---
+++ ThirtyFour.UI/ThirtyFour.UI.Tests/Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThirtyFour.UI.Tests.Tests
+{
+    [TestClass]
+    public class UnitTest1 : BaseTestSuite
+    {
+        protected override string Url
+        {
+            get {
+                return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase), "Resources", "index.html");
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
