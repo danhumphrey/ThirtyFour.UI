b82326eabf071ba28b814e8b7da887511bad1650
Dan Humphrey <danh2000@gmail.com>
2018-02-27 10:53:02 +1100

    added FormElement

---
+++ ThirtyFour.UI/ThirtyFour.UI.Tests/Tests/FormElementTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using ThirtyFour.UI.Form;
+
+namespace ThirtyFour.UI.Tests.Tests
+{
+    [TestClass]
+    public class FormElementTests : BaseTestSuite
+    {
+        public class FormElementImpl : FormElement
+        {
+            public FormElementImpl(IWebElement element) : base(element)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void LabelTextWhenWrapped()
+        {
+            IWebElement el = driver.FindElement(By.Id("rorange"));
+            FormElement radio = new FormElementImpl(el);
+            Assert.AreEqual("Orange", radio.LabelText);
+        }
+
+        [TestMethod]
+        public void LabelTextWithForAttribute()
+        {
+            IWebElement el = driver.FindElement(By.Id("rviolet"));
+            FormElement radio = new FormElementImpl(el);
+            Assert.AreEqual("Violet", radio.LabelText);
+        }
+
+    }
+}
---
+++ ThirtyFour.UI/ThirtyFour.UI/Form/FormElement.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyFour.UI.Form
+{
+    /// <summary>
+    /// Represents a form element with a label
+    /// </summary>
+    public abstract class FormElement : WrappedElement
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="element">The wrapped form element</param>
+        public FormElement(IWebElement element) : base(element) { }
+
+        /// <summary>
+        /// The label text of the element or an empty string if no associated label
+        /// </summary>
+        public string LabelText
+        {
+            get
+            {
+                IReadOnlyCollection<IWebElement> labels = null;
+                IWebElement parentLabel = null;
+
+                //attempt to find label using for=id
+                string id = this.Element.GetAttribute("id");
+                if (id != null)
+                {
+                    labels = this.Driver.FindElements(By.CssSelector(String.Format("label[for='{0}']", id)));
+                }
+
+                if (labels == null || labels.Count == 0) //attempt to find wrapped label element
+                {
+                    parentLabel = this.GetAncestorElement("label");
+                }
+                else
+                {
+                    parentLabel = labels.ElementAt(0);
+                }
+
+                return parentLabel == null ? "" : parentLabel.Text;
+            }
+        }
+    }
+}
