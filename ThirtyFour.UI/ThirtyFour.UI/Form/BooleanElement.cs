9157dc76955e77c01b11c1801195e955e8d807cb
Dan Humphrey <danh2000@gmail.com>
2018-03-01 19:42:25 +1100

    tidying up

--- ThirtyFour.UI/ThirtyFour.UI/Form/BooleanElement.cs
+++ ThirtyFour.UI/ThirtyFour.UI/Form/BooleanElement.cs
@@ -1,9 +1,4 @@
 ﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ThirtyFour.UI.Form
 {
@@ -16,7 +11,7 @@ namespace ThirtyFour.UI.Form
         {
             get
             {
-                return this.Element.Selected;
+                return Element.Selected;
             }
         }
 
