3a60adc6fa0c66365c99a137e184614825928e9b
Dan Humphrey <danh2000@gmail.com>
2018-03-15 20:45:22 +1100

    added Table helper

---
+++ ThirtyFour.UI/ThirtyFour.UI.Tests/Tests/StringCellParserTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using ThirtyFour.UI.Table;
+
+namespace ThirtyFour.UI.Tests.Tests
+{
+    [TestClass]
+    public class StringCellParserTests : BaseTestSuite
+    {
+
+        [TestMethod]
+        public void ParserGetValueReturnsCellValue()
+        {
+            IWebElement cell = driver.FindElement(By.ClassName("first-product"));
+            ICellParser<string> parser = new StringCellParser();
+            Assert.AreEqual("First Test Product", parser.GetValue(cell));
+        }
+    }
+}
