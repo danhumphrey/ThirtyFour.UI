0a5488539c47e5dc5771dd3c1e84c594e61e5eec
Dan Humphrey <danh2000@gmail.com>
2018-03-19 20:44:45 +1100

    added PopupWindow and tests

---
+++ ThirtyFour.UI/ThirtyFour.UI/Window/DefaultWindowSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace ThirtyFour.UI.Window
+{
+    public class DefaultWindowSwitcher : IWindowSwitcher
+    {
+        public void SwitchWindow(IWebDriver driver)
+        {
+            foreach (string handle in driver.WindowHandles)
+            {
+                if (handle != driver.CurrentWindowHandle)
+                {
+                    driver.SwitchTo().Window(handle);
+                    return;
+                }
+            }
+
+            throw new Exception("Unable to find a new Window");
+        }
+    }
+}
---
+++ ThirtyFour.UI/ThirtyFour.UI/Window/PopupWindow.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyFour.UI.Window
+{
+    /// <summary>
+    /// The Window class provides an easy method for dealing with a popup window
+    /// </summary>
+    public class PopupWindow
+    {
+        private IWebDriver driver;
+        private string defaultHandle;
+
+        /// <summary>
+        /// The driver instance
+        /// </summary>
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
+
+            private set
+            {
+                driver = value;
+            }
+        }
+
+        /// <summary>
+        /// The driver instance
+        /// </summary>
+        public string DefaultHandle
+        {
+            get
+            {
+                return defaultHandle;
+            }
+
+            private set
+            {
+                defaultHandle = value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="driver">The IWebDriver instance</param>
+        public PopupWindow(IWebDriver driver):this(driver, new DefaultWindowSwitcher())
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="driver">The IWebDriver instance</param>
+        /// <param name="finder">An instance of IWindowSwitcher</param>
+        public PopupWindow(IWebDriver driver, IWindowSwitcher finder)
+        {
+            this.DefaultHandle = driver.CurrentWindowHandle;
+            this.Driver = driver;
+            finder.SwitchWindow(driver);
+        }
+
+    }
+}
