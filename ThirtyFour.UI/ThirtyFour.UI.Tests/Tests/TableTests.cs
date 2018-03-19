26acb91a0de9ff882df8f4160f411683395ddbb0
Dan Humphrey <danh2000@gmail.com>
2018-03-19 21:00:17 +1100

    added Table tests

---
+++ ThirtyFour.UI/ThirtyFour.UI.Tests/Tests/TableTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+
+namespace ThirtyFour.UI.Tests.Tests
+{
+    using Table = Table.Table;
+
+    [TestClass]
+    public class TableTests : BaseTestSuite
+    {
+        [TestMethod]
+        public void TestHeaderCells()
+        {
+            Table table = new Table(driver.FindElement(By.ClassName("cart")));
+            Assert.AreEqual(4, table.HeaderCells.Count);
+        }
+
+        [TestMethod]
+        public void TestBodyRows()
+        {
+            Table table = new Table(driver.FindElement(By.ClassName("cart")));
+            Assert.AreEqual(3, table.BodyRows.Count);
+        }
+
+        [TestMethod]
+        public void TestGetCellValue()
+        {
+            Table table = new Table(driver.FindElement(By.ClassName("cart")));
+            String cellValue = table.GetCellValue("Product", "First Test Product", "Subtotal");
+            Assert.AreEqual("$9.99", cellValue);
+        }
+    }
+}
