c5e1cbd9b5aa60748ed41f1982d858bcc5e22c52
Dan Humphrey <danh2000@gmail.com>
2018-04-05 22:03:37 +1000

    added UrlMatcher

---
+++ dotnet/ThirtyFour.UI.Tests/Window/UrlMatcherTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using ThirtyFour.UI.Window;
+
+namespace ThirtyFour.UI.Tests.Window
+{
+    [TestClass]
+    public class UrlMatcherTests : BaseTestSuite
+    {
+
+        [TestMethod]
+        public void ExceptionThrownWhenNoMatchingTitle()
+        {
+            try
+            {
+                new UrlMatcher("shjksjd").MatchWindow(driver, 10);
+                Assert.Fail("An exception should have been thrown");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Unable to find Window", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void WindowSwitchedWhenMatchingTitle()
+        {
+
+            driver.FindElement(By.LinkText("Popup")).Click();
+            new UrlMatcher(@"https://github.com/danhumphrey/ThirtyFour.UI").MatchWindow(driver, 10);
+            Assert.AreEqual("https://github.com/danhumphrey/ThirtyFour.UI", driver.Url);
+        }
+
+        [TestMethod]
+        public void WindowSwitchedWhenMatchingTitleOfDelayedWindow()
+        {
+            driver.FindElement(By.LinkText("Delayed Popup")).Click();
+            new UrlMatcher(@"https://github.com/danhumphrey/ThirtyFour.UI").MatchWindow(driver, 10);
+            Assert.AreEqual("https://github.com/danhumphrey/ThirtyFour.UI", driver.Url);
+        }
+    }
+}
---
+++ dotnet/ThirtyFour.UI/Window/UrlMatcher.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyFour.UI.Window
+{
+    /// <summary>
+    /// The UrlMatcher class matches and switches to the first popup window it finds which matches the provided url
+    /// </summary>
+    public class UrlMatcher : IMatcher
+    {
+        private string url;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="title">The title of the window to match</param>
+        public UrlMatcher(string url)
+        {
+            this.url = url;
+        }
+
+        /// <summary>
+        /// Switches to the first popup window found which matches the provided url
+        /// </summary>
+        /// <param name="driver">The IWebDriver instance</param>
+        /// <param name="timeoutInSeconds">The number of seconds to keep trying to find the matching popup window</param>
+        public void MatchWindow(IWebDriver driver, double timeoutInSeconds)
+        {
+            bool found = Utils.RetryUntilTimeout(TimeSpan.FromSeconds(timeoutInSeconds), TimeSpan.FromMilliseconds(100), () => {
+                IReadOnlyCollection<String> windows = driver.WindowHandles;
+
+                foreach (String window in windows)
+                {
+                    driver.SwitchTo().Window(window);
+                    if (driver.Url.Equals(this.url))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            });
+
+            if (!found)
+            {
+                throw new Exception("Unable to find Window");
+            }
+        }
+    }
+}
