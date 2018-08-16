7b8cffef1b5bd06a6fbde5d14241ed793da833ca
Dan Humphrey <danh2000@gmail.com>
2018-08-16 21:07:33 +1000

    tidy up

---
+++ dotnet/ThirtyFour.UI/CleanWebDriverWait.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace ThirtyFour.UI
+{
+    /*
+     * An extension of WebDriverWait which removes then re-applies the ImplicitWait property on the IWebDriver instance
+     */
+    public class CleanWebDriverWait : WebDriverWait
+    {
+        private IWebDriver driver;
+        private TimeSpan implicitWait;
+
+        public CleanWebDriverWait(IWebDriver driver, TimeSpan timeout, TimeSpan implicitWait) : base(driver, timeout)
+        {
+            this.Driver = driver;
+            this.ImplicitWait = implicitWait;
+        }
+
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
+
+            set
+            {
+                driver = value;
+            }
+        }
+
+        public TimeSpan ImplicitWait
+        {
+            get
+            {
+                return implicitWait;
+            }
+
+            set
+            {
+                implicitWait = value;
+            }
+        }
+
+        public new TResult Until<TResult>(Func<IWebDriver, TResult> condition)
+        {
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+            try
+            {
+                var result = base.Until(condition);
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Driver.Manage().Timeouts().ImplicitWait = ImplicitWait;
+            }
+
+        }
+    }
+
+}
