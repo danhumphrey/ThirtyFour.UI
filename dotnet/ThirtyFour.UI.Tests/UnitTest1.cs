919126dc0d9496bc6403251f1cc909307443e2e9
Dan Humphrey <danh2000@gmail.com>
2019-06-25 18:23:05 +1000

    Update .gitignore for .NET Core and Standard Library

---
+++ dotnet/ThirtyFour.UI.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThirtyFour.UI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
