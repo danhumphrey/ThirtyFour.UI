3fc58e88801ef1789d7be9d084fa52782dc985b3
Dan Humphrey <dan.humphrey@accesshq.com>
2019-06-26 05:59:10 +1000

    Add Utils class and unit tests

--- dotnet/ThirtyFour.UI.Tests/Util/UtilsTests.cs
+++ dotnet/ThirtyFour.UI.Tests/Util/UtilsTests.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirtyFour.UI.Util;
+
 namespace ThirtyFour.UI.Tests.Util
 {
-    public class UtilsTests
+    [TestClass]
+    public class UtilsTests : BaseTestSuite
     {
-        public UtilsTests()
+        [TestMethod]
+        public void TestTimeoutReturnsFalse()
         {
+            bool result = Utils.RetryUntilTimeout(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(500), () =>
+            {
+                Thread.Sleep(1000);
+                return false;
+            });
+
+            Assert.IsFalse(result);
         }
+
+        [TestMethod]
+        public void TestSuccessReturnsTrue()
+        {
+            bool result = Utils.RetryUntilTimeout(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(500), () =>
+            {
+                return true;
+            });
+
+            Assert.IsTrue(result);
+        }
+
     }
 }
