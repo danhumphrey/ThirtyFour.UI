80e633be48b2b06862256f390244dbfb0d3a6b5b
Dan Humphrey <danh2000@gmail.com>
2019-06-26 05:59:10 +1000

    Add Utils class and unit tests

--- dotnet/ThirtyFour.UI/Util/Utils.cs
+++ dotnet/ThirtyFour.UI/Util/Utils.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Threading;
+
 namespace ThirtyFour.UI.Util
 {
+    /// <summary>
+    /// Utils class
+    /// </summary>
     public class Utils
     {
-        public Utils()
+        /// <summary>
+        /// Retries a task every 'interval' milliseconds until timeout or success
+        /// </summary>
+        /// <param name="timeout">the timeout</param>
+        /// <param name="interval">the interval for retrying</param>
+        /// <param name="task">the task to retry</param>
+        /// <returns>Boolean - tru if the task succeeded</returns>
+        public static bool RetryUntilTimeout(TimeSpan timeout, TimeSpan interval, Func<bool> task)
         {
+            bool success = false;
+            double elapsed = 0;
+            while ((!success) && (elapsed < timeout.TotalMilliseconds))
+            {
+                Thread.Sleep(interval);
+                elapsed += interval.TotalMilliseconds;
+                success = task();
+            }
+            return success;
         }
+
     }
 }
