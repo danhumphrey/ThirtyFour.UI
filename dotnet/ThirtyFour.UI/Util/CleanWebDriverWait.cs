7f9e9773b8b59587a18d0fca770bf30af961c97e
Dan Humphrey <dan.humphrey@accesshq.com>
2019-06-26 06:41:32 +1000

    Add CleanWebDriverWait and unit tests

---
+++ dotnet/ThirtyFour.UI/Util/CleanWebDriverWait.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ThirtyFour.UI.Util
+{
+    /// <summary>
+    /// An extension of WebDriverWait which removes then re-applies the ImplicitWait property on the IWebDriver instance
+    /// </summary>
+    public class CleanWebDriverWait : WebDriverWait
+    {
+        public CleanWebDriverWait(IWebDriver driver, TimeSpan timeout, TimeSpan implicitWait) : base(driver, timeout)
+        {
+            this.Driver = driver;
+            this.ImplicitWait = implicitWait;
+        }
+
+        public IWebDriver Driver { get; set; }
+
+        public TimeSpan ImplicitWait { get; set; }
+
+        public new TResult Until<TResult>(Func<IWebDriver, TResult> condition)
+        {
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+            try
+            {
+                var result = base.Until(condition);
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Driver.Manage().Timeouts().ImplicitWait = ImplicitWait;
+            }
+
+        }
+    }
+}
