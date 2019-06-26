7597a7e86d04e301bb4245da3b35f951c98fae3b
Dan Humphrey <danh2000@gmail.com>
2019-06-27 08:35:08 +1000

    Add FormElement, BooleanElement and unit tests

---
+++ dotnet/ThirtyFour.UI/Form/BooleanElement.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+
+namespace ThirtyFour.UI.Form
+{
+    public abstract class BooleanElement : FormElement
+    {
+        /// <summary>
+        /// The checked state of the element
+        /// </summary>
+        public bool IsChecked => Element.Selected;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="element">The wrapped element</param>
+        public BooleanElement(IWebElement element) : base(element) { }
+    }
+}
