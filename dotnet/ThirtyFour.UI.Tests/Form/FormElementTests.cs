7b957d55a362b8a79c4596304b5a8d7fcc6a14e7
Dan Humphrey <dan.humphrey@accesshq.com>
2019-06-27 08:35:08 +1000

    Add FormElement, BooleanElement and unit tests

---
+++ dotnet/ThirtyFour.UI.Tests/Form/FormElementTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using ThirtyFour.UI.Form;
+
+namespace ThirtyFour.UI.Tests.Form
+{
+    [TestClass]
+    public class FormElementTests : BaseTestSuite
+    {
+        public class FormElementImpl : FormElement
+        {
+            public FormElementImpl(IWebElement element) : base(element)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void ValuePropertyReturnsCorrectValue()
+        {
+            var el = driver.FindElement(By.Id("rorange"));
+            FormElement radio = new FormElementImpl(el);
+            Assert.AreEqual("orange", radio.Value);
+
+            var inputTextEl = driver.FindElement(By.Name("input-text"));
+            FormElement inputText = new FormElementImpl(inputTextEl);
+            Assert.AreEqual("initial input-text value", inputText.Value);
+            inputTextEl.Clear();
+            inputTextEl.SendKeys("new input-text value");
+            Assert.AreEqual("new input-text value", inputText.Value);
+
+            var inputHiddenEl = driver.FindElement(By.Name("input-hidden"));
+            FormElement inputHidden = new FormElementImpl(inputHiddenEl);
+            Assert.AreEqual("initial input-hidden value", inputHidden.Value);
+
+        }
+
+        [TestMethod]
+        public void LabelTextWhenWrapped()
+        {
+            var el = driver.FindElement(By.Id("rorange"));
+            FormElement radio = new FormElementImpl(el);
+            Assert.AreEqual("Orange", radio.LabelText);
+        }
+
+        [TestMethod]
+        public void LabelTextWithForAttribute()
+        {
+            var el = driver.FindElement(By.Id("rviolet"));
+            FormElement radio = new FormElementImpl(el);
+            Assert.AreEqual("Violet", radio.LabelText);
+        }
+
+    }
+}
---
+++ dotnet/ThirtyFour.UI/Form/FormElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace ThirtyFour.UI.Form
+{
+    /// <summary>
+    /// Represents a form element with a label
+    /// </summary>
+    public abstract class FormElement : WrappedElement
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="element">The wrapped form element</param>
+        protected FormElement(IWebElement element) : base(element) { }
+
+        /// <summary>
+        /// The value of the element
+        /// </summary>
+        public string Value => Element.GetAttribute("value");
+
+        /// <summary>
+        /// The label text of the element or an empty string if no associated label
+        /// </summary>
+        public string LabelText
+        {
+            get
+            {
+                IReadOnlyCollection<IWebElement> labels = null;
+                IWebElement parentLabel;
+
+                //attempt to find label using for=id
+                string id = Element.GetAttribute("id");
+                if (id != null)
+                {
+                    labels = Driver.FindElements(By.CssSelector(String.Format("label[for='{0}']", id)));
+                }
+
+                if (labels == null || labels.Count == 0) //attempt to find wrapped label element
+                {
+                    parentLabel = GetAncestorElement("label");
+                }
+                else
+                {
+                    parentLabel = labels.ElementAt(0);
+                }
+
+                return parentLabel == null ? "" : parentLabel.Text;
+            }
+        }
+    }
+}
