cab312dbbd7353c41a554713a53abd260f264cb7
Dan Humphrey <dan.humphrey@accesshq.com>
2019-06-29 08:34:45 +1000

    Fix flaky Window tests

--- dotnet/ThirtyFour.UI.Tests/Window/DefaultMatcherTests.cs
+++ dotnet/ThirtyFour.UI.Tests/Window/DefaultMatcherTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using ThirtyFour.UI.Window;
 
 namespace ThirtyFour.UI.Tests.Window
@@ -29,7 +30,7 @@ namespace ThirtyFour.UI.Tests.Window
 
             driver.FindElement(By.LinkText("Popup")).Click();
             new DefaultMatcher().MatchWindow(driver, 4);
-            Assert.AreEqual("https://github.com/danhumphrey/ThirtyFour.UI", driver.Url);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlToBe("https://github.com/danhumphrey/ThirtyFour.UI"));
         }
 
         [TestMethod]
@@ -37,7 +38,8 @@ namespace ThirtyFour.UI.Tests.Window
         {
             driver.FindElement(By.LinkText("Delayed Popup")).Click();
             new DefaultMatcher().MatchWindow(driver, 5);
-            Assert.AreEqual("https://github.com/danhumphrey/ThirtyFour.UI", driver.Url);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlToBe("https://github.com/danhumphrey/ThirtyFour.UI"));
+          
         }
     }
 }
