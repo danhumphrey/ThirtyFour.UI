68b9505561d6ebf6c46394467c3229acbd8119e1
Dan Humphrey <dan.humphrey@accesshq.com>
2019-06-29 18:44:32 +1000

    Add TitleMatcher and tests

---
+++ dotnet/ThirtyFour.UI.Tests/Window/TitleMatcherTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using ThirtyFour.UI.Window;
+
+namespace ThirtyFour.UI.Tests.Window
+{
+    [TestClass]
+    public class TitleMatcherTests : BaseTestSuite
+    {
+
+        [TestMethod]
+        public void ExceptionThrownWhenNoMatchingTitle()
+        {
+            try
+            {
+                new TitleMatcher("shjksjd").MatchWindow(driver, 4);
+                Assert.Fail("An exception should have been thrown");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Unable to find Window", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void WindowSwitchedWhenMatchingTitle()
+        {
+
+            driver.FindElement(By.LinkText("Popup")).Click();
+            new TitleMatcher(@"GitHub - danhumphrey/ThirtyFour.UI: A Selenium (element 34) WebDriver UI Library").MatchWindow(driver, 4);
+            Assert.AreEqual("https://github.com/danhumphrey/ThirtyFour.UI", driver.Url);
+        }
+
+        [TestMethod]
+        public void WindowSwitchedWhenMatchingTitleOfDelayedWindow()
+        {
+            driver.FindElement(By.LinkText("Delayed Popup")).Click();
+            new TitleMatcher(@"GitHub - danhumphrey/ThirtyFour.UI: A Selenium (element 34) WebDriver UI Library").MatchWindow(driver, 5);
+            Assert.AreEqual("https://github.com/danhumphrey/ThirtyFour.UI", driver.Url);
+        }
+    }
+}
---
+++ dotnet/ThirtyFour.UI/Window/TitleMatcher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using ThirtyFour.UI.Util;
+
+namespace ThirtyFour.UI.Window
+{
+    /// <summary>
+    /// The TitleMatcher class matches and switches to the first popup window it finds which matches the provided title
+    /// </summary>
+    public class TitleMatcher : IMatcher
+    {
+        private string title;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="title">The title of the window to match</param>
+        public TitleMatcher(string title)
+        {
+            this.title = title;
+        }
+
+        /// <summary>
+        /// Switches to the first popup window found which matches the provided title
+        /// </summary>
+        /// <param name="driver">The IWebDriver instance</param>
+        /// <param name="timeoutInSeconds">The number of seconds to keep trying to find the matching popup window</param>
+        public void MatchWindow(IWebDriver driver, double timeoutInSeconds)
+        {
+            bool found = Utils.RetryUntilTimeout(TimeSpan.FromSeconds(timeoutInSeconds), TimeSpan.FromMilliseconds(100), () => {
+                IReadOnlyCollection<string> windows = driver.WindowHandles;
+
+                foreach (String window in windows)
+                {
+                    driver.SwitchTo().Window(window);
+                    if (driver.Title.Equals(this.title))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            });
+
+            if (!found)
+            {
+                throw new Exception("Unable to find Window");
+            }
+        }
+    }
+}
