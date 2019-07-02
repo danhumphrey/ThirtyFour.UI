4eda28f310c2a8f3c9c544dc3357d444d444e6f7
Dan Humphrey <dan.humphrey@accesshq.com>
2019-07-02 16:52:05 +1000

    Add Table and unit tests

---
+++ dotnet/ThirtyFour.UI.Tests/Table/TableTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace ThirtyFour.UI.Tests.Table
+{
+    using Table = ThirtyFour.UI.Table.Table;
+
+    [TestClass]
+    public class TableTests : BaseTestSuite
+    {
+        [TestMethod]
+        public void TestHeaderCells()
+        {
+            Table table = new Table(driver.FindElement(By.ClassName("cart")));
+            Assert.AreEqual(4, table.HeaderCells.Count);
+        }
+
+        [TestMethod]
+        public void TestBodyRows()
+        {
+            Table table = new Table(driver.FindElement(By.ClassName("cart")));
+            Assert.AreEqual(3, table.BodyRows.Count);
+        }
+
+        [TestMethod]
+        public void TestGetCellValue()
+        {
+            Table table = new Table(driver.FindElement(By.ClassName("cart")));
+            String cellValue = table.GetCellValue("Product", "First Test Product", "Subtotal");
+            Assert.AreEqual("$9.99", cellValue);
+        }
+    }
+}
