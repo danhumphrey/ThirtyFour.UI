066fe568337f876b294a2f1c5cdd614add588090
Dan Humphrey <dan.humphrey@accesshq.com>
2019-07-02 16:32:25 +1000

    Add ChildELementCellParser and unit tests

---
+++ dotnet/ThirtyFour.UI.Tests/Table/ChildElementCellParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using ThirtyFour.UI.Table;
+
+namespace ThirtyFour.UI.Tests.Table
+{
+    [TestClass]
+    public class ChildElementCellParserTests : BaseTestSuite
+    {
+
+        [TestMethod]
+        public void ParserReturnsCorrectElement()
+        {
+            IWebElement cell = driver.FindElement(By.Id("first-action-cell"));
+            ICellParser<IWebElement> parser = new ChildElementCellParser(By.TagName("button"));
+            Assert.AreEqual("first-button", parser.GetValue(cell).GetAttribute("id"));
+        }
+    }
+}
