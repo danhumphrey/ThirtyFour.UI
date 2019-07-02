6b9324e193617a441751a873152355396f6d7b2e
Dan Humphrey <danh2000@gmail.com>
2019-07-02 10:41:33 +1000

    Add StringCellParser and unit tests

---
+++ dotnet/ThirtyFour.UI/Table/ICellParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace ThirtyFour.UI.Table
+{
+    /// <summary>
+    /// Basic strategy for parsing and returning the value of cells found within a table
+    /// </summary>
+    public interface ICellParser<T>
+    {
+        /// <summary>
+        /// Returns the value from the specific table cell element
+        /// </summary>
+        /// <param name="cell">The element representing the table cell table</param>
+        /// <returns>The cell value</returns>
+        T GetValue(IWebElement cell);
+    }
+}
