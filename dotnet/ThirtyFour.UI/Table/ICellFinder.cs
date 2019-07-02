697e5de8fa121a5ec3cd9084e579a00cb9196703
Dan Humphrey <danh2000@gmail.com>
2019-07-02 10:21:17 +1000

    Add ColumnHeaderCellFinder and unit tests

---
+++ dotnet/ThirtyFour.UI/Table/ICellFinder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace ThirtyFour.UI.Table
+{
+    /// <summary>
+    /// Basic strategy api for finding cells within a table
+    /// </summary>
+    public interface ICellFinder
+    {
+        /// <summary>
+        /// Finds and returns a specific table cell
+        /// </summary>
+        /// <param name="tableElement">The element representing the html table</param>
+        /// <returns>The element representing the cell</returns>
+        IWebElement FindCell(IWebElement tableElement);
+    }
+}
