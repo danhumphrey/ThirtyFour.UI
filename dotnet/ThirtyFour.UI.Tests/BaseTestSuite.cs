6029514f9e8d021cefc8d10560b9a12b9be5d1db
Dan Humphrey <danh2000@gmail.com>
2019-07-05 08:48:31 +1000

    Remove and sort usings

--- dotnet/ThirtyFour.UI.Tests/BaseTestSuite.cs
+++ dotnet/ThirtyFour.UI.Tests/BaseTestSuite.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
 namespace ThirtyFour.UI.Tests
 {
--- dotnet/ThirtyFour.UI/Table/Table.cs
+++ dotnet/ThirtyFour.UI/Table/Table.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using OpenQA.Selenium;
 
 namespace ThirtyFour.UI.Table
