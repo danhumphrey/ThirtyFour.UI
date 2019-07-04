a5d4e69248d95633355264dc3eedfe1d5f219810
Dan Humphrey <danh2000@gmail.com>
2019-07-04 20:29:33 +1000

    Add ChildElementCellParser

---
+++ java/src/main/java/com/github/danhumphrey/thirtyfour/ui/table/ChildElementCellParser.java
@@ -0,0 +1,32 @@
+package com.github.danhumphrey.thirtyfour.ui.table;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Parsers a child element from a table cell
+ *
+ */
+public class ChildElementCellParser implements CellParser<WebElement> {
+
+	protected By childLocator;
+	
+	/**
+	 * Constructor
+	 * @param childLocator the locator used to find the child element
+	 */
+	public ChildElementCellParser(By childLocator) {
+		this.childLocator = childLocator;
+	}
+		
+	/**
+	 * returns a child element from the cell
+	 * @param WebElement the cell to parse
+	 * @return WebElement the child element
+	 */
+	@Override
+	public WebElement getValue(WebElement cell) {
+		return cell.findElement(childLocator);
+	}
+
+}
