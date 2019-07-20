efa456f6833a7bbfb2f0ee4833266619f8057a1a
Dan Humphrey <danh2000@gmail.com>
2019-07-21 07:53:55 +1000

    Update UtilsTests

--- dotnet/ThirtyFour.UI.Tests/Util/UtilsTests.cs
+++ dotnet/ThirtyFour.UI.Tests/Util/UtilsTests.cs
@@ -31,5 +31,39 @@ namespace ThirtyFour.UI.Tests.Util
             Assert.IsTrue(result);
         }
 
+        [TestMethod]
+        public void TestFalseResultNotReturnedBeforeTimeout()
+        {
+            DateTime start = DateTime.UtcNow;
+
+            bool result = Utils.RetryUntilTimeout(TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(100), () =>
+            {
+                Thread.Sleep(50);
+                return false;
+            });
+
+            DateTime end = DateTime.UtcNow;
+            TimeSpan diff = end.Subtract(start);
+
+            Assert.IsTrue(diff.Milliseconds > 500);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestSuccessResultReturnedBeforeTimeout()
+        {
+            DateTime start = DateTime.UtcNow;
+
+            bool result = Utils.RetryUntilTimeout(TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(100), () =>
+            {
+                return true;
+            });
+
+            DateTime end = DateTime.UtcNow;
+            TimeSpan diff = end.Subtract(start);
+
+            Assert.IsTrue(diff.Milliseconds < 200);
+            Assert.IsTrue(result);
+        }
     }
 }
