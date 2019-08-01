b1d0344ab17415929f926b8b883e0bb88b31d605
Dan Humphrey <danh2000@gmail.com>
2019-08-01 19:16:05 +1000

    Add TitleMatcher and unit tests

---
+++ java/src/main/java/com/github/danhumphrey/thirtyfour/ui/window/TitleMatcher.java
@@ -0,0 +1,62 @@
+package com.github.danhumphrey.thirtyfour.ui.window;
+
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import org.openqa.selenium.WebDriver;
+
+import com.github.danhumphrey.thirtyfour.ui.util.Utils;
+
+/**
+ * The DefaultMatcher matches the first other browser window it finds
+ *
+ */
+public class TitleMatcher implements Matcher {
+	
+	private String title;
+	
+	/**
+	 * Constructor
+	 * @param title the title of the window you want to match
+	 */
+	public TitleMatcher(String title) {
+		this.title = title;
+	}
+	
+	public class MatcherTask implements Callable<Boolean> {
+	    
+		private WebDriver driver;
+		
+		public MatcherTask(WebDriver driver) {
+			this.driver = driver;
+		}
+		
+		public Boolean call() throws InterruptedException {
+			
+			String currentHandle = driver.getWindowHandle();
+			
+	    	Set<String> handles = driver.getWindowHandles();
+	    	for(String handle : handles) {
+	    		
+	    		driver.switchTo().window(handle);
+	    		if(driver.getTitle().equals(title)) {
+	    			return true;
+	    		}
+	    		driver.switchTo().window(currentHandle);
+	    	}
+	    	return false;
+	    }
+	}
+	
+	@Override
+	public void matchWindow(WebDriver driver, long timeoutInMilliseconds) throws Exception {
+		
+		boolean found = Utils.retryUntilTimeout(timeoutInMilliseconds, 100, new MatcherTask(driver));
+		
+		if (!found) {
+            throw new Exception("Unable to find a matching window");
+        }
+			
+	}
+
+}
---
+++ java/src/test/java/com/github/danhumphrey/thirtyfour/ui/test/window/TitleMatcherTests.java
@@ -0,0 +1,61 @@
+package com.github.danhumphrey.thirtyfour.ui.test.window;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.github.danhumphrey.thirtyfour.ui.test.BaseTestSuite;
+import com.github.danhumphrey.thirtyfour.ui.window.TitleMatcher;
+
+public class TitleMatcherTests extends BaseTestSuite {
+
+	@Test
+	public void testExceptionThrownWhenNoMatchingTitle() {
+
+		WebDriver driver = getDriver();
+		String currentHandle = driver.getWindowHandle();
+
+		try {
+			new TitleMatcher("shjksjd").matchWindow(driver, 4);
+			Assertions.fail("An exception should have been thrown");
+		} catch (Exception e) {
+			Assertions.assertEquals("Unable to find a matching window", e.getMessage());
+		} finally {
+			quitExtraDrivers(currentHandle);
+		}
+	}
+
+	@Test
+    public void WindowSwitchedWhenMatchingTitle() throws Exception
+    {
+    	WebDriver driver = getDriver();
+		String currentHandle = driver.getWindowHandle();
+
+		driver.findElement(By.linkText("Popup")).click();
+		
+		try {
+	        new TitleMatcher("GitHub - danhumphrey/ThirtyFour.UI: A Selenium (element 34) WebDriver UI Library for C# and Java").matchWindow(driver, 1000);
+	        Assertions.assertEquals("https://github.com/danhumphrey/ThirtyFour.UI", driver.getCurrentUrl());
+		} finally {
+			quitExtraDrivers(currentHandle);
+		}
+        
+    }
+
+	@Test
+    public void testWindowSwitchedWhenMatchingTitleOfDelayedWindow() throws Exception
+    {
+    	WebDriver driver = getDriver();
+		String currentHandle = driver.getWindowHandle();
+		
+        driver.findElement(By.linkText("Delayed Popup")).click();
+        
+        try {
+	        new TitleMatcher("GitHub - danhumphrey/ThirtyFour.UI: A Selenium (element 34) WebDriver UI Library for C# and Java").matchWindow(driver, 5000);
+	        Assertions.assertEquals("https://github.com/danhumphrey/ThirtyFour.UI", driver.getCurrentUrl());
+		} finally {
+			quitExtraDrivers(currentHandle);
+		}
+    }
+}
