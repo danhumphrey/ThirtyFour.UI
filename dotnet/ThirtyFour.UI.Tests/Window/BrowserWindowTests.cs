53516c96e6568e907a0dd23392038ef395a7c433
Dan Humphrey <danh2000@gmail.com>
2019-08-01 12:27:35 +1000

    Update BrowserWindowTests

--- dotnet/ThirtyFour.UI.Tests/Window/BrowserWindowTests.cs
+++ dotnet/ThirtyFour.UI.Tests/Window/BrowserWindowTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
@@ -9,25 +10,41 @@ namespace ThirtyFour.UI.Tests.Window
     [TestClass]
     public class WindowTests : BaseTestSuite
     {
+        private void QuitExtraDrivers(String currentHandle)
+        {
+            ReadOnlyCollection<String> handles = driver.WindowHandles;
+
+            foreach (String handle in handles)
+            {
+                if (!handle.Equals(currentHandle))
+                {
+                    driver.SwitchTo().Window(handle);
+                    driver.Quit();
+                }
+            }
+        }
 
         [TestMethod]
         public void DriverEqualsCurrentDriver()
         {
+            string currentHandle = driver.CurrentWindowHandle;
+
             driver.FindElement(By.LinkText("Popup")).Click();
             BrowserWindow window = new BrowserWindow(driver);
             Assert.AreEqual(driver, window.Driver);
+
+            QuitExtraDrivers(currentHandle);
         }
 
         [TestMethod]
         public void ExceptionThrownWhenNoPopupExists()
         {
-            BrowserWindow window = null;
             string currentHandle = driver.CurrentWindowHandle;
 
             try
             {
-                window = new BrowserWindow(driver);
-                Assert.Fail();
+                _ = new BrowserWindow(driver);
+                Assert.Fail("Expected exception not thrown");
             }
 #pragma warning disable RECS0022 // A catch clause that catches System.Exception and has an empty body
             catch
@@ -35,22 +52,34 @@ namespace ThirtyFour.UI.Tests.Window
             {
                
             }
+            finally {
+                QuitExtraDrivers(currentHandle);
+            }
+
         }
 
         [TestMethod]
         public void DefaultHandleNotEqualsDriverHandleWhenPopupDoesExist()
         {
+            string currentHandle = driver.CurrentWindowHandle;
+
             driver.FindElement(By.LinkText("Popup")).Click();
             BrowserWindow window = new BrowserWindow(driver);
             Assert.AreNotEqual(driver.CurrentWindowHandle, window.DefaultHandle);
+
+            QuitExtraDrivers(currentHandle);
         }
 
         [TestMethod]
         public void WindowSwitchesToCorrectWindowWhenNoFinderIsProvided()
         {
+            string currentHandle = driver.CurrentWindowHandle;
+
             driver.FindElement(By.LinkText("Popup")).Click();
             BrowserWindow window = new BrowserWindow(driver);
             new WebDriverWait(driver, TimeSpan.FromSeconds(2)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlToBe("https://github.com/danhumphrey/ThirtyFour.UI"));
+
+            QuitExtraDrivers(currentHandle);
         }
     }
 }
